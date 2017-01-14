986493bf6f0192f9d06683b21c2fed0f87233086
SuperLinguini <milind.lingineni@gmail.com>
2017-01-14 10:32:49 -0500

    Finished cost function

--- machine-learning-ex2/costFunction.m
+++ machine-learning-ex2/costFunction.m
@@ -20,7 +20,10 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
+%Vectorized implementation
+h = sigmoid(X * theta);
+J = (1/m) * sum(-y' * log(h) - (1 - y)' * log(1 - h));
+grad = (1/m) * X' * (h - y);
 
 % =============================================================
 
